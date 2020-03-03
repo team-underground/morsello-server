f976e078ac460ebdb72cbb6e979c7a6d07e645e6
mithicher <mithnehu@gmail.com>
2020-03-03 13:58:48 +0530

    reset, github merged to master

--- app/Like.php
+++ app/Like.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     public $timestamps = false;
+
+    protected $fillable = [
+        "user_id",
+        "bit_id"
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bit()
+    {
+        return $this->belongsTo(Bit::class);
+    }
 }
