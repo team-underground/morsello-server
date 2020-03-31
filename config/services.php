9416422e1bbdd2080c6f32361db8bafad7380189
abhishek sarmah <abhisheksarmah660@gmail.com>
2020-03-31 19:20:54 +0530

    github login redirect issue fixed 1

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => env('FRONT_URL') . '/login/github/callback',
+        'redirect' => env('APP_URL') . '/login/github/callback',
     ],
 
 ];
